88a6558448ea7020274e7f07081944c17e826eff
Nick Bolten <nbolten@gmail.com>
2015-11-01 23:32:11 -0800

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'coral',
+    'author': 'Nick Bolten',
+    'url': 'https://github.com/klavinslab/aquarium-api-python',
+    'download_url': 'https://github.com/klavinslab/aquarium-api-python.git',
+    'author_email': 'nbolten _at_ gmail',
+    'version': '0.1.0',
+    'install_requires': ['argparse', 'requests'],
+    'packages': ['aquarium'],
+    'scripts': [],
+    'name': 'aquarium',
+    'license': 'Copyright University of Washington'
+}
+
+setup(**config)
