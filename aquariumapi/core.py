6d43316e8ed66202066ccb29ceb3bf16121ad5a2
Nick Bolten <nbolten@gmail.com>
2015-11-25 15:13:55 -0800

    Added TaskModel class, simplified instantiation of it and SampleModel

--- aquariumapi/core.py
+++ aquariumapi/core.py
@@ -2,58 +2,53 @@ import requests
 
 
 class AquariumAPI(object):
-    """Reusable Aquarium API connector."""
-    def __init__(self, url, login, key, project=None):
-        """
+    '''Reusable Aquarium API connector.'''
+    def __init__(self, url, login, key):
+        '''
         :param url: URL of the API, usually (aquarium base URL)/api.
         :type url: str
         :param login: Aquarium user login name.
         :type login: str
         :param key: Aquarium API key.
         :type key: str
-        :param project: Aquarium project (e.g. cool_yeast_project) with which
-                        to associate all submissions from this API instance.
-        :type project: str
 
-        """
+        '''
         self.url = url
         self.login = login
         self.key = key
-        self.project = project
 
         # Attempt a connection - tests the url, login, and key
         try:
             test_request = self._request({}, {})
         except requests.ConnectionError:
-            raise requests.ConnectionError("Could not connect to URL")
-        if test_request["result"] != "ok":
-            raise requests.ConnectionError("{}".format(test_request["errors"]))
+            raise requests.ConnectionError('Could not connect to URL')
+        if test_request['result'] != 'ok':
+            raise requests.ConnectionError('{}'.format(test_request['errors']))
 
-    def find(self, model, where_query=None, limit=None):
-        """Find entries in a model matching a query on table columns.
+    def find(self, model, where=None, limit=None):
+        '''Find entries in a model matching a query on table columns.
 
         :param model: Model in the database to search.
         :type model: str
-        :param where_query: A query of column: comparator key-value pairs,
+        :param where: A query of column: comparator key-value pairs,
                             e.g. {'id': 5}
-        :type where_query: dict
+        :type where: dict
         :param limit: Limits the number of queries to return (synonymous with
                      SQL LIMIT).
         :type limit: int
 
-        """
-        method = "find"
-        run_args = {"model": model}
-        if where_query is not None:
-            run_args["where"] = where_query
+        '''
+        method = 'find'
+        run_args = {'model': model}
+        if where is not None:
+            run_args['where'] = where
         if limit is not None:
-            run_args["limit"] = limit
+            run_args['limit'] = limit
 
         return self._request(method, run_args)
 
-    def create_sample(self, sample_type, name, description, fields,
-                      project=None):
-        """Create a new sample (adds new row to Sample table).
+    def create_sample(self, sample_type, name, description, fields, project):
+        '''Create a new sample (adds new row to Sample table).
 
         :param sample_type: The name of the 'SampleType', e.g. 'PCR' or
                             'Fragment'.
@@ -69,18 +64,16 @@ class AquariumAPI(object):
                         submission.
         :type project: str
 
-        """
-        if project is None:
-            project = self.project
-        method = "create"
-        run_args = {"model": "sample", "type": sample_type, "name": name,
-                    "project": project, "description": description,
-                    "fields": fields}
+        '''
+        method = 'create'
+        run_args = {'model': 'sample', 'type': sample_type, 'name': name,
+                    'project': project, 'description': description,
+                    'fields': fields}
 
         return self._request(method, run_args)
 
-    def create_task(self, name, task_type, specification, project=None):
-        """Creates a new task (adds new row to Tasks table).
+    def create_task(self, name, task_type, specification):
+        '''Creates a new task (adds new row to Tasks table).
 
         :param name_task: Task type name (e.g. 'PCR')
         :type name_task: str
@@ -93,68 +86,66 @@ class AquariumAPI(object):
                         submission.
         :type project: str
 
-        """
-        if project is None:
-            project = self.project
-        json_task_prototype = self.find("task_prototype", {"name": task_type})
-        task_prototype_id = json_task_prototype["rows"][0]["id"]
+        '''
+        json_task_prototype = self.find('task_prototype', {'name': task_type})
+        task_prototype_id = json_task_prototype['rows'][0]['id']
 
-        method = "create"
-        run_args = {"model": "task",
-                    "name": name,
-                    "status": "waiting",
-                    "task_prototype_id": task_prototype_id,
-                    "specification": specification}
+        method = 'create'
+        run_args = {'model': 'task',
+                    'name': name,
+                    'status': 'waiting',
+                    'task_prototype_id': task_prototype_id,
+                    'specification': specification}
 
         return self._request(method, run_args)
 
     def drop_by_names(self, model, names):
-        """Drop database entries by name.
+        '''Drop database entries by name.
 
         :param model: Model from which to drop entries.
         :type model: str
         :param names: A list of entry names (unique identifiers) to drop.
         :type names: list
 
-        """
-        method = "drop"
-        run_args = {"model": model, "names": names}
+        '''
+        method = 'drop'
+        run_args = {'model': model, 'names': names}
 
         return self._request(method, run_args)
 
     def drop_by_ids(self, model, ids):
-        """Drop database entries by ID.
+        '''Drop database entries by ID.
 
         :param model: Model from which to drop entries.
         :type model: str
         :param ids: A list of entry IDs (unique identifiers) to drop.
         :type ids: list
 
-        """
-        method = "drop"
-        run_args = {"model": model, "ids": ids}
+        '''
+        method = 'drop'
+        run_args = {'model': model, 'ids': ids}
 
         return self._request(method, run_args)
 
     def modify(self, query_params):
-        """Not yet implemented."""
+        '''Not yet implemented.'''
         # TODO: Write once this is documented
-        raise NotImplementedError("The 'modify' method has no API docs.")
+        raise NotImplementedError('The "modify" method has no API docs.')
 
     def _request(self, method, args):
-        """Reusable method for making requests to the API"""
+        '''Reusable method for making requests to the API'''
         data = {}
-        data["login"] = self.login
-        data["key"] = self.key
-        run = {"method": method, "args": args}
-        data["run"] = run
+        data['login'] = self.login
+        data['key'] = self.key
+        run = {'method': method, 'args': args}
+        data['run'] = run
 
         r = requests.post(self.url, json=data)
         # TODO: validate request error code
         if r.status_code != 200:
-            print "Returned status code: %{}".format(r.status_code)
+            print 'Returned status code: %{}'.format(r.status_code)
         else:
             json = r.json()
-            # TODO: validate result ("status": OK)
+            # TODO: validate result ('status': OK)
             # TODO: provide a useful response message?
             return json
--- aquariumapi/models.py
+++ aquariumapi/models.py
@@ -1,13 +1,16 @@
 '''Classes for interacting with the API models of Aquarium servers.'''
+import json
 
 
 class SampleModel(object):
     '''Accesses samples via the API.'''
-    def __init__(self, api, sample_type, fields=None, id_=None, project=None):
+    def __init__(self, api, sample_type):
         '''
         :param api: An instance of the Aquarium API client.
         :type api: aquariumapi.AquariumAPI
-        :param sample_type: The exact 'name' for the sample type definition.
+        :param sample_type: The exact name for the sample type definition. This
+                            is used to look up the info (including fields) for
+                            this sample type.
         :type sample_type: str
         :param fields: A list of lists of field-type pairs (used solely as
                        information for the user) for the sample type, e.g.
@@ -17,38 +20,48 @@ class SampleModel(object):
         :param id_: The id of the sample type (used solely as information for
                     the user).
         :type id_: int
-        :param project: The project with which to associate any samples created
-                        by this object. Can be set on a per-creation basis
-                        as well.
-        :type project: str
 
         '''
         self.api = api
         self.sample_type = sample_type
-        self.project = project
-        self.fields = fields
-        self.id = id_
+        sample_type_info = api.find('sample_type',
+                                    {'name': self.sample_type})['rows'][0]
+
+        self.id = sample_type_info['id']
+        self.fields = []
+        # Hard-coded field number is 8
+        for i in range(1, 8):
+            if sample_type_info['field{}type'.format(i)] != 'not used':
+                field_name = str(sample_type_info['field{}name'.format(i)])
+                field_type = str(sample_type_info['field{}type'.format(i)])
+                self.fields.append([field_name, field_type])
 
     def info(self):
         '''Displays sample type, id, and field information.'''
         return {'sample_type': self.sample_type, 'id': self.id,
                 'fields': self.fields}
 
-    def find(self, where_query=None, limit=None):
+    def find(self, where=None, limit=None):
         '''Finds samples of this type using the same syntax as the built-in
         AquariumAPI methods.
 
-        :param where_query: Query for selecting subsets of this sample type.
-                            Input type is identical to that for
-                            AquariumAPI.find
-        :type where_query: dict
+        :param where: Query for selecting subsets of this sample type. Input
+                      type is identical to that for AquariumAPI.find
+        :type where: dict
         :param limit: Limits the number of returned rows to this number.
         :type limit:
 
         '''
-        return self.api.find('sample', where_query, limit)
+        if where is None:
+            where = {}
 
-    def create(self, name, description, fields, project=None):
+        # If self.id is set, add it to the 'where' query.
+        if self.id is not None:
+            where['sample_type_id'] = self.id
+
+        return self.api.find('sample', where=where, limit=limit)
+
+    def create(self, name, description, fields, project):
         '''Creates a new row of this sample type in the database.
 
         :param name: Name of the sample.
@@ -63,8 +76,6 @@ class SampleModel(object):
         :type project: str
 
         '''
-        if project is None:
-            project = self.project
 
         return self.api.create_sample(self.sample_type, name, description,
                                       fields, project)
@@ -96,20 +107,34 @@ class SampleModel(object):
 
 
 class TaskModel(object):
-    def __init__(self, api, task_type, project=None):
+    def __init__(self, api, task_type):
         self.api = api
         self.task_type = task_type
-        self.project = project
 
-    def find(self, where_query=None, limit=None):
-        return self.api.find('task', where_query, limit)
+        task_prototype = api.find('task_prototype',
+                                  {'name': task_type})['rows'][0]
+        self.id = task_prototype['id']
+        # Get prototype
+        # FIXME: why isn't it already JSON?
+        self.prototype = json.loads(task_prototype['prototype'])
 
-    def create(self, name, specification, project=None):
-        if project is None:
-            project = self.project
+    def info(self):
+        '''Displays sample type, id, and field information.'''
+        return {'task_type': self.task_type, 'id': self.id,
+                'prototype': self.prototype}
 
-        return self.api.create_task(name, self.task_type, specification,
-                                    project)
+    def find(self, where=None, limit=None):
+        if where is None:
+            where = {}
+
+        # If self.id is set, add it to the 'where' query.
+        if self.id is not None:
+            where['task_prototype_id'] = self.id
+
+        return self.api.find('task', where=where, limit=limit)
+
+    def create(self, name, specification):
+        return self.api.create_task(name, self.task_type, specification)
 
     def drop(self):
         raise NotImplementedError
@@ -117,33 +142,25 @@ class TaskModel(object):
     def modify(self, args):
         raise NotImplementedError
 
+    def __repr__(self):
+        return '{} TaskModel'.format(self.task_type)
 
-def get_sample_definitions(api, project=None):
+
+def get_sample_definitions(api):
     sample_types = api.find('sample_type')['rows']
-
-    def get_sample_model(sample_type, project=None):
-        # Create instances of SampleModel
-        name = sample_type['name']
-        id_ = sample_type['id']
-        # Hard-coded field number is 8
-        n_fields = 8
-        fieldinfo = []
-        for i in range(1, n_fields):
-            if sample_type['field{}type'.format(i)] != 'not used':
-                field_name = str(sample_type['field{}name'.format(i)])
-                field_type = str(sample_type['field{}type'.format(i)])
-                fieldinfo.append([field_name, field_type])
-
-        # clsname = ''.join([word.capitalize() for word in name.split(' ')])
-
-        cls = SampleModel(api, name, fields=fieldinfo, id_=id_,
-                          project=project)
-
-        return cls
 
     models = {}
     for sample_type in sample_types:
-        models[sample_type['name']] = get_sample_model(sample_type,
-                                                       project=project)
+        models[sample_type['name']] = SampleModel(api, sample_type['name'])
+
+    return models
+
+
+def get_task_definitions(api):
+    task_prototypes = api.find('task_prototype')['rows']
+
+    models = {}
+    for task_prototype in task_prototypes:
+        models[task_prototype['name']] = TaskModel(api, task_prototype['name'])
 
     return models
